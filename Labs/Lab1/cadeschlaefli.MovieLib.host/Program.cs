87b3e2913014d30448160f6484abcf52ac48a3e3
cslaf <cade.schlaefli339@my.tccd.edu>
2018-01-17 21:23:21 -0600

    Lab1 initial commit

---
+++ Labs/Lab1/cadeschlaefli.MovieLib.host/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadeschlaefli.MovieLib.host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+}
