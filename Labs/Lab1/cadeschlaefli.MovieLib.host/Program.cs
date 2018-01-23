4179dcfd62ade4516fe38e41b1f056e5bd511ad5
cslaf <35430865+cslaf@users.noreply.github.com>
2018-01-23 05:57:11 -0600

    Finished extended program

--- Labs/Lab1/cadeschlaefli.MovieLib.host/Program.cs
+++ Labs/Lab1/cadeschlaefli.MovieLib.host/Program.cs
@@ -10,17 +10,50 @@ namespace cadeschlaefli.MovieLib.host
     {
         static void Main( string[] args )
         {
-            
+            bool quit = false;
+            int pick;
+            string userInput;
 
+            string[] mainMenuCommands = { "1. List Movies", "2. Add Movie", "3. Remove Movie", "4. Quit" };
+
+            MovieList MovieList = new MovieList();
+
+            do
+            {
+                foreach (string c in mainMenuCommands)
+                {
+                    Console.WriteLine(c + "\n");
+                }
+                userInput = Console.ReadLine();
+
+                if (Int32.TryParse(userInput, out pick))
+                {
+                    switch (pick)
+                    {
+                        case 1:
+                        MovieList.listMovies();
+                        break;
+                        case 2:
+                        MovieList.addMovie();
+                        break;
+                        case 3:
+                        MovieList.removeMovie();
+                        break;
+                        case 4:
+                        quit = true;
+                        break;
+                        default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                    }
+                }
+
+            } while (!quit);
+            
         }
     }
 
-    class commandLineMenu
-    {
-        List<String> commands;
-        List<String> descriptions;
 
-    }
 
 
     class Movie
@@ -45,6 +78,11 @@ namespace cadeschlaefli.MovieLib.host
                     break;
                 }
             } while (true);
+        }
+
+        public string getTitle()
+        {
+            return title;
         }
 
         void setDescription()
@@ -74,6 +112,7 @@ namespace cadeschlaefli.MovieLib.host
         void setOwnership()
         {
             string toCheck;
+            bool exit = false;
             do
             {
                 Console.WriteLine("Do you own the Movie? (Y/N)\n");
@@ -82,17 +121,19 @@ namespace cadeschlaefli.MovieLib.host
                 {
                         case "y":
                         case "Y":
-                        ownership = true;
-                        break;
+                    ownership = true;
+                    exit = true;
+                    break;
                         case "n":
                         case "N":
-                        ownership = false;
-                        break;
+                    ownership = false;
+                    exit = true;
+                    break;
                         default:
-                            Console.WriteLine("Enter actual option");
+                    Console.WriteLine("Enter (Y/N)\n");
                     break;
                 } 
-            } while (true);
+            } while (!exit);
         }
 
         public void setInfo()
@@ -106,16 +147,36 @@ namespace cadeschlaefli.MovieLib.host
             setOwnership();
         }
         
+        public void listInfo()
+        {
+            Console.WriteLine(title + "\n" + description + "\nRun Length = " + length + "\n");
+            if (ownership)
+            {
+                Console.WriteLine("Is Owned\n");
+            } else
+            {
+                Console.WriteLine("Not Owned\n");
+            }
+        }
 
     }
 
 
-    class MovieController
+    class MovieList
     {
-        List<Movie> mList;
+        List<Movie> mList = new List<Movie>();
         public void listMovies()
         {
-
+            if(mList.Count != 0)
+            {
+                foreach(Movie m in mList)
+                {
+                    m.listInfo();
+                }
+            } else
+            {
+                Console.WriteLine("No Movies in list");
+            }
         }
         public void addMovie()
         {
@@ -125,7 +186,85 @@ namespace cadeschlaefli.MovieLib.host
         }
         public void removeMovie()
         {
+            string userInput;
+            int movieNumber;
 
+            if (mList.Count == 0)
+            {
+                Console.WriteLine("No Movies in list");
+                return;
+            }
+
+            for (int i = 0; i < mList.Count ; i++)
+            {
+                Console.WriteLine(i + ". " + mList[i].getTitle()+"\n");
+            }
+
+            do
+            {
+                if (mList.Count > 1)
+                {
+                    Console.WriteLine("Remove which movie?(0-" + (mList.Count - 1) + ")\n");
+                    userInput = Console.ReadLine();
+                    if (Int32.TryParse(userInput, out movieNumber))
+                    {
+                        if ((movieNumber >= 0) && (movieNumber <= mList.Count - 1))
+                        {
+                            bool exit = false;
+                            do
+                            {
+                                Console.WriteLine("Are you sure you want to delete the movie (Y/N)?\n");
+                                userInput = Console.ReadLine();
+                                switch (userInput)
+                                {
+                                    case "y":
+                                    case "Y":
+                                    mList.RemoveAt(movieNumber);
+                                    Console.WriteLine("\nMovie Removed");
+                                    exit = true;
+                                    break;
+                                    case "n":
+                                    case "N":
+                                    Console.WriteLine("\nCanceled");
+                                    exit = true;
+                                    break;
+                                    default:
+                                    Console.WriteLine("\nEnter (Y/N)");
+                                    break;
+                                }
+                            } while (!exit);
+                            break;
+                        }
+                    }
+                } else
+                {
+                    bool exit = false;
+                    do
+                    {
+                        Console.WriteLine("Remove the only movie?(Y/N)\n");
+                        userInput = Console.ReadLine();
+                        switch (userInput)
+                        {
+                            case "y":
+                            case "Y":
+                            mList.RemoveAt(0);
+                            Console.WriteLine("\nMovie Removed");
+                            exit = true;
+                            break;
+                            case "n":
+                            case "N":
+                            Console.WriteLine("\nCanceled");
+                            exit = true;
+                            break;
+                            default:
+                            Console.WriteLine("\nEnter (Y/N)");
+                            break;
+                        }
+                    } while (!exit);
+                    break;
+
+                }
+            } while (true);
         }
     }
 }
